ba33627b319a1113ac35f58ddca172a8a407ef0d
emmett1 <emmett1.2miligrams@gmail.com>
2017-12-16 15:02:51 +0800

    updated

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,5 +5,7 @@ RCD_DIR=/etc/rc.d
 RCCONF_DIR=/etc/conf.d
 
 mkdir -pv ${DESTDIR}{${RC_DIR},${RCD_DIR},${RCCONF_DIR}}
+
 install -m644 inittab ${DESTDIR}${RC_DIR}
 install -m754 rc.{conf,local,multi,single,shutdown,sysinit} ${DESTDIR}${RC_DIR}
+install -m644 conf.d/functions ${DESTDIR}${RCCONF_DIR}
