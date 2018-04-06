30ab556d8b1621908a1b0d20a71974b46d5b2319
emmett1 <emmett1.2miligrams@gmail.com>
2018-04-06 23:49:00 +0800

    updated

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,5 +8,5 @@ mkdir -pv ${DESTDIR}{${RC_DIR},${RCD_DIR},${RCCONF_DIR}}
 
 install -m644 inittab ${DESTDIR}${RC_DIR}
 install -m754 rc ${DESTDIR}${RC_DIR}
-install -m754 rc.d/rc.{localnet,modules,mountfs,multi,setclock,shutdown,single,swap,sysklogd,udev} ${DESTDIR}${RCD_DIR}
+install -m754 rc.d/rc.{local,localnet,modules,mountfs,multi,setclock,shutdown,single,swap,sysklogd,udev} ${DESTDIR}${RCD_DIR}
 install -m644 conf.d/{clock,modules} ${DESTDIR}${RCCONF_DIR}
